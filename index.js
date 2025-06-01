82b32f823eea9f70279aa2586fd2356882cfcdd4
stetupdev <jacobstetup@tuta.io>
2025-06-01 20:35:44 +0000

    git commit -m

--- index.js
+++ index.js
@@ -5,13 +5,22 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// Set Content Security Policy header
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "connect-src 'self' https://botforge-endpoints.onrender.com;"
+  );
+  next();
+});
+
 app.post("/vibex-webhook", (req, res) => {
   console.log("Received webhook ping:", req.body);
   res.status(200).json({ message: "Webhook received" });
 });
 
 app.get("/", (req, res) => {
-  res.status(200).send("VibeX webhook server is up.");
+  res.status(200).send("Botforge-Endpoints server is running.");
 });
 
 app.listen(PORT, () => {
